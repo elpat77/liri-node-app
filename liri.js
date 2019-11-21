18abdf39c5808a22f5a144aeeae4d652bceddb4d
elpat7 <aguilarpat7@gmail.com>
2019-11-20 17:03:37 -0800

    working spotify liri

--- liri.js
+++ liri.js
@@ -1,53 +1,53 @@
+var fs = require('fs');
 dotenv = require("dotenv").config();
-
-
 function spotifySearch(searchedSong) {
     var Spotify = require('node-spotify-api');
     var keys = require("./keys");
     var spotify = new Spotify(keys.spotify);
 
-    const url = 'https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx';
 
-    var songName = process.argv.slice(3).join('+');
+    var Spotify = require('node-spotify-api');
+
+    // search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+    var songName = process.argv[2];
     if (searchedSong) {
         songName = searchedSong;
     } else if (!songName) {
         songName = 'the sign';
     }
+    spotify.search({ type: 'track', query: songName, limit: 7 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var songDetails = data.tracks.items[0];
+        // console.log(songDetails);
+        var songSearched = (songName);
+        console.log("I am looking for", songSearched);
 
-    spotify
-        .request(url).
-        then(result => {
-            // console.log(process.argv);
-            console.log("data", result);
-            var artist = (result.artists[0].name);
-            console.log("The Artist name is", artist)
-            var album = (result.album.name);
-            console.log("The album name is", album)
-            var song = (result.name);
-            console.log("The songs name is", song)
-            var previewLink = (result.preview_url);
-            console.log("If a songs preview is available, it can be found here: ", previewLink)
-            var songLink = (result.external_urls.spotify)
-            console.log("If a songs preview is available, it can be found here: ", songLink)
+        var artist = (songDetails.artists[0].name);
+        console.log("The Artist name is", artist);
+
+        var album = (songDetails.album.name);
+        console.log("The album name is", album);
+
+        var song = (songDetails.name);
+        console.log("The songs name is", song);
+
+        var previewLink = (songDetails.preview_url);
+        console.log("If a songs preview is available, it can be found here: ", previewLink);
+
+        var songLink = (songDetails.external_urls.spotify)
+        console.log("If a songs preview is available, it can be found here: ", songLink);
+
+        var searchResult = (songSearched + '\n' + artist + '\n' + album + '\n' + song + '\n' + previewLink + '\n' + songLink + '\n')
+        console.log(searchResult);
+
+        fs.appendFile('log.txt', searchResult, function (err) {
+            if (err) {
+                return console.log(error);
+            }
         })
-        .catch(function (err) {
-            console.error('Error occurred: ' + err);
-        });
+
+    });
 }
 spotifySearch()
-
-
-// search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
-
-// const Params = { type: 'track', query: process.argv[2], limit: 20 };
-
-// spotify.search(Params, function (err, data) {
-//     if (err) {
-//         return console.log('Error occurred: ' + err);
-//     }
-
-//     console.log(data);
-// });
-
-// console.log(process.argv);
--- spotifyFun.js
+++ spotifyFun.js
@@ -1,2 +1,45 @@
-const name = "Pat"
-module.exports = name;
+dotenv = require("dotenv").config();
+function spotifySearch(searchedSong) {
+    var Spotify = require('node-spotify-api');
+    var keys = require("./keys");
+    var spotify = new Spotify(keys.spotify);
+
+
+    var Spotify = require('node-spotify-api');
+
+    // search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+    var songName = process.argv[2];
+    if (searchedSong) {
+        songName = searchedSong;
+    } else if (!songName) {
+        songName = 'the sign';
+    }
+    spotify.search({ type: 'track', query: songName, limit: 7 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var songDetails = data.tracks.items[0];
+        // console.log(songDetails);
+        var songSearched = (songName);
+        console.log("I am looking for", songSearched)
+
+        var artist = (songDetails.artists[0].name);
+        console.log("The Artist name is", artist)
+
+        var album = (songDetails.album.name);
+        console.log("The album name is", album)
+
+        var song = (songDetails.name);
+        console.log("The songs name is", song)
+
+        var previewLink = (songDetails.preview_url);
+        console.log("If a songs preview is available, it can be found here: ", previewLink)
+
+        var songLink = (songDetails.external_urls.spotify)
+        console.log("If a songs preview is available, it can be found here: ", songLink)
+
+        var searchResult = (songSearched + '\n' + artist + '\n' + album + '\n' + song + '\n' + previewLink + '\n' + songLink + '\n')
+        console.log(searchResult)
+    });
+}
+spotifySearch()
