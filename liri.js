caa273ad37f857d7cca0c1a3024377dc87328299
elpat7 <aguilarpat7@gmail.com>
2019-11-26 16:46:32 -0800

    working spotify-this-song

--- liri.js
+++ liri.js
@@ -1,12 +1,11 @@
 var fs = require('fs');
 dotenv = require("dotenv").config();
 const axios = require("axios");
-console.log(process.argv[2]);
 var functionCalled = process.argv[2];
 
 
 function movieSearch() {
-    var movieTitle = require("./functions")
+    var movieTitle = require("./movieFunct");
     var movieUrl =
         `http://www.omdbapi.com/?t=${movieTitle()}&y=&plot=short&apikey=trilogy`;
 
@@ -20,7 +19,7 @@ function movieSearch() {
         var moviePlot = ("Plot -" + " " + res.data.Plot);
         var movieActors = ("Cast -" + " " + res.data.Actors);
 
-        var movieSearchResult = (movieTitle + '\n' + movieYear + '\n' + movieIMDBRating + '\n' + movieRottenRating + '\n' + movieCountry + '\n' + movieLanguage + '\n' + moviePlot + '\n' + movieActors + '\n');
+        var movieSearchResult = ('\n' + movieTitle + '\n' + movieYear + '\n' + movieIMDBRating + '\n' + movieRottenRating + '\n' + movieCountry + '\n' + movieLanguage + '\n' + moviePlot + '\n' + movieActors + '\n');
         console.log(movieSearchResult);
         fs.appendFile('log.txt', movieSearchResult, function (err) {
             if (err) {
@@ -31,44 +30,39 @@ function movieSearch() {
 }
 
 function spotifySearch() {
+    // var spotifyTitle = require("./songFunct");
+    // console.log("title", spotifyTitle);
     var Spotify = require('node-spotify-api');
     var keys = require("./keys");
     var spotify = new Spotify(keys.spotify);
     var songInput = '';
 
-    if (process.argv.length > 2) {
-        var songName = []
-
-        for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv.length > 3) {
+        var songName = [];
+        for (let i = 3; i < process.argv.length; i++) {
             songName.push(process.argv[i])
         }
         songInput = (songName.join('+'));
         // console.log(songInput);
 
     } else {
-        songInput = 'the+sign';
+        songInput = 'The Sign';
+        console.log("SI", songInput);
     }
+
     spotify.search({ type: 'track', query: songInput, limit: 7 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
         var songDetails = data.tracks.items[0];
-
-        var artist = (songDetails.artists[0].name);
-
-        var album = (songDetails.album.name);
-
-        var song = (songDetails.name);
-
-
-        var previewLink = (songDetails.preview_url);
-        console.log("If a songs preview is available, it can be found here: ", previewLink);
-
-        var songLink = (songDetails.external_urls.spotify)
-        console.log("If a songs preview is available, it can be found here: ", songLink);
-
-        var songSearchResult = (songInput + '\n' + artist + '\n' + album + '\n' + song + '\n' + previewLink + '\n' + songLink + '\n');
-        // console.log(songSearchResult);
+        // console.log(songDetails);
+        var song = ("Song Name - " + " " + songDetails.name);
+        var artist = ("Artist Name - " + " " + songDetails.artists[0].name);
+        var album = ("Album Name - " + " " + songDetails.album.name);
+        var previewLink = ("If a songs preview is available, it can be found here: " + " " + songDetails.preview_url);
+        var songLink = ("A link to the song can be found here:" + " " + songDetails.external_urls.spotify)
+        var songSearchResult = ('\n' + song + '\n' + artist + '\n' + album + '\n' + previewLink + '\n' + songLink + '\n');
+        console.log(songSearchResult);
 
         fs.appendFile('log.txt', songSearchResult, function (err) {
             if (err) {
@@ -78,7 +72,6 @@ function spotifySearch() {
 
     });
 }
-
 
 if (functionCalled === 'movie-this') {
     movieSearch();
--- songFunct.js
+++ songFunct.js
@@ -1,16 +1,14 @@
-const movieTitle = () => {
+const spotifyTitle = () => {
 
     const arguements = process.argv;
     let argArr = [];
 
     for (let i = 3; i < arguements.length; i++) {
         argArr.push(arguements[i]);
-        console.log(process.argv[2]);
-
     }
     //will join the array by If inserting a + sign instead of the empty spaces
     return argArr.join("+");
 
 };
 
-module.exports = movieTitle
+module.exports = spotifyTitle
